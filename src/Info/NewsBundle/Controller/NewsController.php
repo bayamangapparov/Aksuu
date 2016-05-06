89b89efadb1043b73869c0853ddb2b7edf808f8e
Emil <emilbeksulaymanov>
2016-05-07 01:44:56 +0600

    Show news

--- src/Info/NewsBundle/Controller/NewsController.php
+++ src/Info/NewsBundle/Controller/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace Info\NewsBundle\Controller;
 
+use Info\NewsBundle\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class NewsController extends Controller
@@ -11,4 +12,12 @@ class NewsController extends Controller
         $news = $this->getDoctrine()->getRepository('InfoNewsBundle:News')->findAll();
         return $this->render('InfoNewsBundle:News:listMainNews.html.twig', array('entities' => $news));
     }
+
+    public function getNewsAction(News $id)
+    {
+        return $this->render('InfoNewsBundle:News:getNews.html.twig',
+            array('news' => $id));
+    }
+
+
 }
